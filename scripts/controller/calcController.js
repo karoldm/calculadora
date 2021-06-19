95db2734f99de69adbc6d0cc21a2502336c746e6
karol marques <karoldm1579@gmail.com>
2021-06-18 22:08:50 -0300

    adicionar botão CE e Delete

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,30 +1,30 @@
-class CalcController{
-    constructor(){
+class CalcController {
+    constructor() {
         this.displayCalc = document.querySelector('#display');
         this.Display;
-        this.Operation=[];
+        this.Operation = [];
         this.lastNumber;
         this.lastOperator;
         this.init();
     }
 
-    init(){
-        this.lastNumber = " ";
+    init() {
+        this.lastNumber = "";
         this.InitButtons();
         this.setDisplay("");
     }
 
-    InitButtons(){ //função para adicionar evento aos buttons
+    InitButtons() { //função para adicionar evento aos buttons
         let buttons = document.querySelectorAll('.btn');
-        buttons.forEach((btn)=>{
-            btn.addEventListener('click', ()=>{
+        buttons.forEach((btn) => {
+            btn.addEventListener('click', () => {
                 this.ExecButtons(btn.textContent);
-            })   
+            })
         });
     }
 
-    ExecButtons(button){
-        switch(button){
+    ExecButtons(button) {
+        switch (button) {
             case '0':
             case '1':
             case '2':
@@ -37,16 +37,16 @@ class CalcController{
             case '8':
             case '9':
                 this.AddOperation(button);
-            break;
+                break;
             case 'Del':
                 this.DeletLastDig();
-            break;
+                break;
             case 'C':
                 this.DeletAll();
-            break;
+                break;
             case 'CE':
                 this.DeletLastOperation();
-            break;
+                break;
             case '+':
             case '-':
                 this.AddOperation(button);
@@ -62,67 +62,77 @@ class CalcController{
                 break;
             case 'x²':
                 this.lastNumber = this.lastNumber * this.lastNumber;
-                this.setLastNumber();   
+                this.setLastNumber();
                 break;
             case '√x':
                 this.lastNumber = Math.sqrt(this.lastNumber);
-                this.setLastNumber();       
+                this.setLastNumber();
                 break;
             case '1/x':
-                this.lastNumber = 1/this.lastNumber;
+                this.lastNumber = 1 / this.lastNumber;
                 this.setLastNumber();
                 break;
             case '+/-':
-                this.lastNumber = -1*this.lastNumber;
+                this.lastNumber = -1 * this.lastNumber;
                 this.setLastNumber();
                 break;
             case '=':
-                if(isNaN(this.Operation[this.Operation.length -1])) this.Operation.push(this.lastNumber);
+                if (isNaN(this.Operation[this.Operation.length - 1])) this.Operation.push(this.lastNumber);
                 this.Calc();
-            break;
+                break;
             case '.':
                 this.addDot();
-            break;
+                break;
         }
     }
 
-    addDot(){
-        if(!this.isOperator(this.Operation[this.Operation.length])){ //ponto após operador é inválido
+    addDot() {
+        //ponto após operador e número com mais de um ponto são inválidos
+        if (!this.isOperator(this.Operation[this.Operation.length]) && this.lastNumber.indexOf(".") == -1) {
+            if (!this.lastNumber) this.setDisplay('0.');
+            else this.setDisplay(this.getDisplay() + ".");
             this.lastNumber += ".";
-            this.setDisplay(this.getDisplay() + ".");
         }
     }
 
-    setLastNumber(){
-        if(!this.isOperator(this.Operation[this.Operation.length - 1])) this.Operation.pop();
+    setLastNumber() {
+        if (!this.isOperator(this.Operation[this.Operation.length - 1])) this.Operation.pop();
         this.Operation.push(this.lastNumber);
         this.setDisplay(this.lastNumber);
     }
 
-    CalcPorcentagem(){
-        this.lastNumber = this.lastNumber/100;
+    CalcPorcentagem() {
+        this.lastNumber = this.lastNumber / 100;
         let value = this.Operation[this.Operation.length - 2];
         this.lastNumber = this.lastNumber * value;
         this.Operation.push(this.lastNumber);
         this.setLastNumber();
     }
 
-    DeletLastOperation(){
-
+    DeletLastOperation() {
+        if (this.lastNumber) {
+            this.lastNumber = "";
+            this.setDisplay("");
+        }
+        else this.Operation.pop();
     }
 
-    DeletLastDig(){
-        
+    DeletLastDig() {
+        if (this.lastNumber) {
+            this.lastNumber = this.lastNumber.substr(0, (this.lastNumber.length - 1))
+            this.setDisplay('');
+            this.setDisplay(this.lastNumber);
+        }
     }
 
-    DeletAll(){
+    DeletAll() {
         this.Operation = [];
         this.lastNumber = " ";
         this.setDisplay(this.Operation);
     }
-    
-    isOperator(value){
-        switch(value){
+
+    isOperator(value) {
+        switch (value) {
             case '+':
             case '-':
             case 'x':
@@ -137,46 +147,46 @@ class CalcController{
         return false;
     }
 
-    AddOperation(value){
-        if(isNaN(value)){ //se value não for um número
-            if(this.lastNumber != " " && isNaN(this.Operation[this.Operation.length -1])) this.Operation.push(this.lastNumber); 
-            //se houver algum digito em lastNumber e se o último elemento do array não for um número, lastNumber é adicionado ao array 
-            if(this.Operation.length == 0) return; //o primeiro digito não pode ser um operador 
-            //caso o usuário queira um número negativo como primeiro digito precisa usar o botão "+/-"
-            if(this.isOperator(this.Operation[this.Operation.length -1])) return; //expressões com dois operadores consecutivos são inválidas
-            this.Operation.push(value);
-            this.lastNumber = "";
-        }
+    AddOperation(value) {
+        if (isNaN(value)) { //se value não for um número
+            if (this.lastNumber) this.Operation.push(this.lastNumber);
+            if (this.Operation.length != 0) { //o primeiro digito não pode ser um operador
+                if (this.isOperator(this.Operation[this.Operation.length - 1])) { //se forem digiados dois operadores
+                    this.Operation.pop(); //troca de operadores
+                }
+                this.Operation.push(value);
+                this.lastNumber = "";
+            }
+        } //
         else { //value é um número
-            if(this.Operation.length == 1) { //apos uma operação o array possui um único elemento: o resultado
-                //caso o usuário entre com outro número o resultado anterior exibido na tela deve sumir
-                this.Operation = [];
-                this.lastNumber = " ";
-                this.setDisplay(this.lastNumber);
-            }//if
+            if (this.Operation.length != 0) {
+                if (this.isOperator(this.Operation[this.Operation.length - 1]) && !this.lastNumber) {
+                    this.setDisplay("");
+                }//if
+            }
             this.lastNumber += value;
+            this.setDisplay(this.getDisplay() + value);
         }//else
-        if(this.Operation.length >3) { //se o array possui três elementos a operação é realiazada automaticamente
-            this.Operation.pop(); 
+        if (this.Operation.length > 3) { //se o array possui três elementos a operação é realiazada automaticamente
+            this.lastOperator = this.Operation.pop();
             this.Calc();
-            return;
         }//if
-        this.setDisplay(this.getDisplay() + value);
     }
 
-    Calc(){
+    Calc() {
         let operacao = this.Operation.join(' '); //transformando o array em uma operação js
-        this.lastNumber = eval(operacao); //validando a operação
-        this.setDisplay(this.lastNumber);//exibindo resultado
+        let result = eval(operacao); //validando a operação
+        this.setDisplay(result);//exibindo resultado
         this.Operation = []; //zerando array de operações
-        this.Operation.push(this.lastNumber); 
+        this.Operation.push(result); //adicionando resultado 
+        this.Operation.push(this.lastOperator); //adicionando o ultimo operador que foi removido 
     }
 
-    setDisplay(value){
+    setDisplay(value) {
         this.displayCalc.innerHTML = value;
     }
 
-    getDisplay(){
+    getDisplay() {
         return (this.displayCalc.value);
     }
 }
